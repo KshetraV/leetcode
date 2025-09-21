0d6d40abd87f4fba37334af9e4766f92e0b51cdd
Kshetra <82812377+KshetraV@users.noreply.github.com>
2025-09-21 17:52:39 +0530

    Time: 11 ms (91.86%) | Memory: 48.4 MB (76.44%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,28 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        List<List<Integer>> listOfLists = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        List<List<Integer>> list = new ArrayList<>();
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
             hs.put(nums[i],hs.getOrDefault(nums[i],0)+1);
         }
-        for(int i=0;i<=nums.length;i++){
-            listOfLists.add(new ArrayList<>());
+
+        for(int i=0;i<=n;i++){
+            list.add(new ArrayList<>());
         }
 
-        for(Map.Entry<Integer, Integer> entry: hs.entrySet()){
+        for(Map.Entry<Integer, Integer> entry:hs.entrySet()){
             int key=entry.getKey();
             int index=entry.getValue();
-            listOfLists.get(index).add(key);
+            list.get(index).add(key);
         }
-        int []res=new int[k];
-        int idx=0;
-        for (int i=listOfLists.size()-1; i>=0; i--) {
-            for (int j = 0; j < listOfLists.get(i).size(); j++) {
-                res[idx++]=listOfLists.get(i).get(j);
-                if(idx==k) return res;
+        int [] res = new int[k];
+        int index=0;
+        for(int i=list.size()-1;i>=0;i--){
+            for(int j=0;j<list.get(i).size();j++){
+                res[index++]=list.get(i).get(j);
+                if(k==index) return res;
             }
-       }
-       return res;
+        }
+
+        return res;
     }
     
 }
