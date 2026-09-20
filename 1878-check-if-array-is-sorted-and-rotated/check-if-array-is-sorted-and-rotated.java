eb2e3c84b9c567413c54d3d96657b33dd37494bd
Kshetra <82812377+KshetraV@users.noreply.github.com>
2026-09-20 22:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (46.92%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,11 +1,29 @@
 class Solution {
     public boolean check(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>nums[(i+1)%nums.length]){
-                count++;
-            }
+        int count=1;
+        int l=nums.length;
+        if(l==count) return true;
+        int[]dup=new int[l*2];
+        for(int i=0;i<l;i++){
+            dup[i]=nums[i];
         }
-        return count<=1;
+        int j=0;
+        for(int i=nums.length;i<l*2;i++){
+            dup[i]=nums[j++];
+        }
+
+    
+        for(int i=1;i<dup.length;i++){
+            if(dup[i]>=dup[i-1]){
+                count++;
+                if(count==l) return true;
+            }
+            
+            else count=1;
+            // System.out.print(count);
+        }
+        return count==l;
     }
 }
+
+// 3 4 5 1 2 3 4 5 1 2 
